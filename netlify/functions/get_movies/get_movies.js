7382cb585a9908bc17f6f359ce100bb582160f42
Ivan <luis.herrerae@outlook.com>
2024-01-29 15:19:54 -0600

    initial commit

---
+++ netlify/functions/get_movies/get_movies.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require("mongodb")
+require('dotenv').config()
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI)
+const connection = mongoClient.connect()
+
+exports.handler = async () => { 
+  try {
+    const database = (await connection).db(process.env.MONGODB_DATABASE)
+    const collection = database.collection(process.env.MONGODB_COLLECTION)
+    //FIXME add pagination support
+    const data = await collection.find({}).limit(10).toArray()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: error.toString()
+    }
+  }
+ }
+
